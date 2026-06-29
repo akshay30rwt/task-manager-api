56059852daded4aa26bf57e6c822d9c9040b2add
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-29 17:37:30 +0530

    fixed process.env.MONGO_URI. Task Manager API with MVC architecture is ready

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
-mongoose.connect(MONGO_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
 }).catch((error) => {
     console.error('Connection failed', error);
