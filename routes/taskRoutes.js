86687cd89eb6b084d9598d46edc1d5ece1b394c4
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-29 17:22:55 +0530

    Add GET, POST, PUT, PATCH, DELETE routes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const { addTask, getTasks, getTaskById, updateTask, deleteTask, taskStatus, getTasksByStatus, getTasksSortedByDueDate } = require('../controllers/taskController');
+
+router.post('/', addTask);
+router.get('/', getTasks);
+router.get('filter/:status', getTasksByStatus);
+router.get('sorted/duedate', getTasksSortedByDueDate);
+router.get('/:id', getTaskById);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
+router.patch('/:id', taskStatus);
+
+module.exports = router;
